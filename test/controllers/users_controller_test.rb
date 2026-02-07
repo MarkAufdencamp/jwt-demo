9a6ba38f4ba7fbbf61619cc1f5d1effa43a6ad37
Mark Aufdencamp <markaufdencamp@Kimberlys-Mac-mini.local>
2026-02-06 21:38:28 -0500

    fix(test): Resolve fixture collision in UsersControllerTest

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,13 +3,13 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should create user" do
     assert_difference("User.count") do
-      post users_url, params: { user: { email: "test@example.com", password: "password123", username: "testuser" } }, as: :json
+      post users_url, params: { user: { email: "newuser@example.com", password: "password123", username: "newuser" } }, as: :json
     end
 
     assert_response :created
     json_response = JSON.parse(response.body)
-    assert_equal "testuser", json_response["username"]
-    assert_equal "test@example.com", json_response["email"]
+    assert_equal "newuser", json_response["username"]
+    assert_equal "newuser@example.com", json_response["email"]
     assert_nil json_response["password_digest"]
   end
 
